a041a8a5d7123c422f963d81340a951a187cc29d
Anurag <anuragmishra11904@gmail.com>
2026-03-18 17:33:50 +0530

    Pallindrome

---
+++ Pallindrome.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+// pallindrom of String (Brute force approach)
+
+// void Brutepallindrome(){
+
+//     string str="MADAM";
+//     string rev="";
+
+//     for(int i=str.length()-1;i>=0;i--){
+//         rev += str[i];
+//     }
+
+//    if(str == rev){
+//     cout<<"Pallindrome";
+//    }else{
+//     cout<< "not a pallindrome";
+//    }
+// }
+
+
+// Optimal approach 0(1)
+void Optimal(){
+string str="madam";
+
+int l=0,r=str.length()-1;
+bool ispallindrome=true;
+
+while(l<r){
+    // swap(str[l],str[r]);
+    if(str[l]!=str[r]){
+    ispallindrome=false;
+    break;}
+    l++;
+    r--;
+
+}
+
+if (ispallindrome){
+    cout<<"Pallindrome";
+}else{
+    cout<<"Not a pallindrome";
+}
+}
+
+
+int main(){
+    // Brutepallindrome();
+
+    Optimal();
+    return 0;
+
+}
