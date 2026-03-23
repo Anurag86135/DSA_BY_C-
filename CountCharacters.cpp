a4b19efe8f8b7d4cd8d4c8062996c43e8d687aa6
Anurag <anuragmishra11904@gmail.com>
2026-03-23 17:52:43 +0530

    Count Characters

---
+++ CountCharacters.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str;
+    cout<<"Enter the String :"<<endl;
+    getline(cin,str);
+    int count=0; 
+cout<<"Your string is :"<<str;
+    for(int i=0;i<str.length();i++){
+        if(str [i]!=' '){
+        count++;
+        
+        }
+    }
+    cout<<"Number of Characters in the given string is :"<< count <<endl;
+    return 0;
+    
+
+    
+}
