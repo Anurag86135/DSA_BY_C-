64d7675758b7658a5d206590711624f6fc69997d
Anurag <anuragmishra11904@gmail.com>
2026-03-27 22:39:20 +0530

    Check strings are same or not

---
+++ EqualStrings.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    string str1="hello";
+    string str2="hell";
+
+    if(str1==str2){
+        cout<<"Strings are equal";
+
+    }else{
+        cout<<"Strings are not equal";
+    }
+    return 0;
+}
