766f20c15f7804f41b836bdf3d58f16932aad978
Anurag <anuragmishra11904@gmail.com>
2026-03-27 22:51:49 +0530

    check strings are equal or not

--- EqualStrings.cpp
+++ EqualStrings.cpp
@@ -1,15 +1,39 @@
 #include<iostream>
 using namespace std;
 
+// Optimal approach
+// int main(){
 
+//     string str1="hello";
+//     string str2="hell";
+
+//     if(str1==str2){
+//         cout<<"Strings are equal";
+
+//     }else{
+//         cout<<"Strings are not equal";
+//     }
+//     return 0;
+// }
+
+// Brute force approach
+
+bool areEqual(string str1,string str2){
+    if(str1.length()!=str2.length()){
+        return false;
+    }
+    for(int i=0;i<str1.length();i++){
+        if(str1[i]!=str2[i]){
+            return false;
+        }
+    }
+    return true;
+}
 int main(){
-
     string str1="hello";
-    string str2="hell";
-
-    if(str1==str2){
+    string str2="helloji";
+    if(areEqual(str1,str2)){
         cout<<"Strings are equal";
-
     }else{
         cout<<"Strings are not equal";
     }
