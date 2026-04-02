7cd14dd7b704fe6cec0a39931545681676a6dc76
Anurag <anuragmishra11904@gmail.com>
2026-04-03 00:19:23 +0530

    Common elements

--- Arrays/CommonElement.cpp
+++ Arrays/CommonElement.cpp
@@ -3,24 +3,49 @@
 #include<iostream>
 using namespace std;
 
+// int main(){
+
+//     int arr1[]={2,3,4,8};
+//     int arr2[]={4,3,5,67,8};
+
+//     int n1= sizeof(arr1)/sizeof(arr1[0]);
+//     int n2= sizeof(arr2)/sizeof(arr2[0]);
+
+//     for(int i=0;i<n1;i++){
+//         for(int j=0;j<n2;j++){
+//             if(arr1[i]==arr2[j]){
+//                 cout<<arr1[i]<<" ";
+//                 break;
+//             }
+//         }
+//     }
+//     return 0;
+// }
+
+
+//Optimal approach two pointer approach
+
 int main(){
-
     int arr1[]={2,3,4,8};
-    int arr2[]={4,3,5,67,8};
+    int arr2[]={3,4,5,8,67};
 
-    int n1= sizeof(arr1)/sizeof(arr1[0]);
-    int n2= sizeof(arr2)/sizeof(arr2[0]);
+    int n1 = sizeof(arr1)/sizeof(arr1[0]);
+    int n2 = sizeof(arr2)/sizeof(arr1[0]);
 
-    for(int i=0;i<n1;i++){
-        for(int j=0;j<n2;j++){
-            if(arr1[i]==arr2[j]){
-                cout<<arr1[i]<<" ";
-                break;
-            }
+    int i=0,j=0;
+
+    while(i<n1 && j<n2){
+        if(arr1[i] == arr2[j]){
+            cout<<arr1[i]<<" ";
+            i++;
+            j++;
+        }
+        else if(arr1[i]<arr2[j]){
+            i++;
+        }
+        else{
+            j++;
         }
     }
     return 0;
 }
-
-
-// 
