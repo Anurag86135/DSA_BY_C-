1ca947fe235266a22b9acf7a65a26dc18db48d2a
Anurag <anuragmishra11904@gmail.com>
2026-04-04 14:13:21 +0530

    Frequency

--- Arrays/FreqArray.cpp
+++ Arrays/FreqArray.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<unordered_map>
 using namespace std;
 
 // int main(){
@@ -18,32 +19,69 @@ using namespace std;
 // }
 
 // Brute forcce
+// int main(){
+//     int arr[]={1,2,2,3,1,4,2};
+//     int n=7;
+
+//     for(int i=0;i<n;i++){
+//         // Check pehle aya hai ya nahi
+        
+//         bool visited =false;
+//         for(int k=0;k<i;k++){
+//             if(arr[i] == arr[k]){
+//                 visited =true;
+
+//             }
+//         }
+//         if(visited == true){
+//             continue; //skip
+//         }
+
+//         // Count karo
+//         int count =0;
+//         for(int j=0;j<n;j++){
+//             if(arr[i] ==arr[j]){
+//                 count++;
+//             }
+//         }
+//         cout<<arr[i]<<" -> "<<count <<endl;
+//     }
+//     return 0;
+// }
+
+// Optimal
 int main(){
-    int arr[]={1,2,2,3,1,4,2};
+    int arr[]={1,2,2,2,3,1,4,2};
     int n=7;
 
+    unordered_map<int,int>freq;
+
+    // count frequency
+
     for(int i=0;i<n;i++){
-        // Check pehle aya hai ya nahi
-        
-        bool visited =false;
-        for(int k=0;k<i;k++){
-            if(arr[i] == arr[k]){
-                visited =true;
+        freq[arr[i]]++;
 
-            }
-        }
-        if(visited == true){
-            continue; //skip
-        }
-
-        // Count karo
-        int count =0;
-        for(int j=0;j<n;j++){
-            if(arr[i] ==arr[j]){
-                count++;
-            }
-        }
-        cout<<arr[i]<<" -> "<<count <<endl;
     }
+
+    cout<<"Frequencies:\n";
+    for(auto it :freq){
+        cout<<it.first<<" -> "<<it.second << endl;
+    }
+
+    // find the max frequency element
+
+    int maxFreq =0;
+    int ans =-1;
+
+    for(auto it : freq){
+        if(it.second > maxFreq){
+            maxFreq=it.second;
+            ans =it.first;
+        }
+    }
+    cout<<"\nMax Frequencey Element:\n";
+    cout<<"Element:"<<ans<<endl;
+    cout<<"Frequency:"<<maxFreq<<endl;
+
     return 0;
 }
