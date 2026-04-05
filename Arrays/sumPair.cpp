a4829c8b1c121d0fefae9da72fd68b22fb5fb4a8
Anurag <anuragmishra11904@gmail.com>
2026-04-05 23:05:35 +0530

    Pair of sum

--- Arrays/sumPair.cpp
+++ Arrays/sumPair.cpp
@@ -1,16 +1,37 @@
 #include<iostream>
+#include<unordered_set>
 using namespace std;
-int main(){
-int arr[]={2,7,11,15};
-int target=9;
-int n=sizeof(arr)/sizeof (arr[0]);
 
-for(int i=0;i<n;i++){
-    for(int j=i+1;j<n;j++){
-        if(arr[i]+arr[j] == target){
-            cout<<"( "<<arr[i]<<" , "<<arr[j]<<" )";
+// int main(){
+// int arr[]={2,7,11,15};
+// int target=9;
+// int n=sizeof(arr)/sizeof (arr[0]);
+
+// for(int i=0;i<n;i++){
+//     for(int j=i+1;j<n;j++){
+//         if(arr[i]+arr[j] == target){
+//             cout<<"( "<<arr[i]<<" , "<<arr[j]<<" )";
+//         }
+//     }
+// }
+// return 0;
+// }
+
+int main(){
+    int arr[]={2,7,11,15};
+    int target=9;
+
+    int n= sizeof(arr)/sizeof(arr[0]);
+
+    unordered_set<int> s;
+
+    for(int i=0;i<n;i++){
+        int complement =target - arr[i];
+
+        if(s.find(complement)!=s.end()){
+            cout<<"( " <<complement <<" , "<<arr[i]<<" )";
         }
+        s.insert(arr[i]);
     }
-}
-return 0;
+    return 0;
 }
