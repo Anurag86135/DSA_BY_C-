5196920522df0a48758fd0fa13c83c805b72fbe9
Anurag <anuragmishra11904@gmail.com>
2026-04-05 23:12:37 +0530

    MAx Difference

---
+++ Arrays/MAxDiff.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+    int arr[]={2,3,10,6,4,8,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+int maxDiff =INT_MIN;
+
+for(int i=0;i<n;i++){
+    for(int j=i+1;j<n;j++){
+        maxDiff =max(maxDiff, arr[j] -arr[i]);
+    }
+}
+cout<<maxDiff;
+return 0;
+}
+
