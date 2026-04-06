ac2438ad6a742cb0401dd60a075770429e8cef9e
Anurag <anuragmishra11904@gmail.com>
2026-04-06 15:14:56 +0530

    finding MAX Differnece

--- Arrays/MAxDiff.cpp
+++ Arrays/MAxDiff.cpp
@@ -1,17 +1,34 @@
 #include<iostream>
 #include<climits>
 using namespace std;
+// int main(){
+//     int arr[]={2,3,10,6,4,8,1};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     int maxDiff =INT_MIN;
+
+// for(int i=0;i<n;i++){
+//     for(int j=i+1;j<n;j++){
+//         maxDiff =max(maxDiff, arr[j] -arr[i]);
+//     }
+// }
+
+// cout<<maxDiff;
+// return 0;
+// }
+
+
 int main(){
     int arr[]={2,3,10,6,4,8,1};
-    int n = sizeof(arr)/sizeof(arr[0]);
-int maxDiff =INT_MIN;
+    int n= sizeof(arr) /sizeof(arr[0]);
 
-for(int i=0;i<n;i++){
-    for(int j=i+1;j<n;j++){
-        maxDiff =max(maxDiff, arr[j] -arr[i]);
+    int minElement =arr[0];
+    int maxDiff =INT_MIN;
+
+    for(int i=0;i<n;i++){
+        maxDiff=max(maxDiff,arr[i] - minElement);
+        minElement =min(minElement,arr[i]);
+
     }
+    cout<<maxDiff;
+    return 0;
 }
-cout<<maxDiff;
-return 0;
-}
-
