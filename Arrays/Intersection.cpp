0d95d7b2271eb151b2e4823493a8268e9da4cb03
Anurag <anuragmishra11904@gmail.com>
2026-04-08 11:29:32 +0530

    Intersection

--- Arrays/Intersection.cpp
+++ Arrays/Intersection.cpp
@@ -25,30 +25,54 @@ using namespace std;
 
 // Optimal Appraoch
 
+// int main(){
+// int arr1[]={1,2,3,4,5};
+// int arr2[]={3,4,5,6,7};
+
+// int n1= sizeof(arr1)/sizeof(arr1[1]);
+// int n2= sizeof(arr2)/sizeof (arr2[1]);
+
+// sort(arr1,arr1+n1);
+// sort(arr2,arr2+n2);
+
+// int i=0,j=0;
+// cout<<" Intersection: "<<endl;
+// while(i<n1 && j<n2){
+//     if(arr1[i] < arr2[j]){
+//         i++;
+//     }
+//     else if(arr1[i]> arr2[j]){
+//         j++;
+//     }
+//     else{// arr1[]==arr2[]
+//         cout<<arr1[i]<<" ";
+//         i++;
+//         j++;
+//     }
+// }
+// return 0;
+// }
+
+// Hashmap approach
+
 int main(){
-int arr1[]={1,2,3,4,5};
-int arr2[]={3,4,5,6,7};
+    int arr1[]={1,2,3,4,5};
+    int arr2[]={3,4,5,6,7};
 
-int n1= sizeof(arr1)/sizeof(arr1[1]);
-int n2= sizeof(arr2)/sizeof (arr2[1]);
+    int n1 = sizeof(arr1)/sizeof(arr1[0]);
+    int n2 = sizeof(arr2)/sizeof(arr2[0]);
 
-sort(arr1,arr1+n1);
-sort(arr2,arr2+n2);
+    unordered_set<int> s;
+    // Store arr1 elements 
+    for(int i=0;i<n1;i++){
+        s.insert(arr1[i]);
+    }
 
-int i=0,j=0;
-cout<<" Intersection: "<<endl;
-while(i<n1 && j<n2){
-    if(arr1[i] < arr2[j]){
-        i++;
+    cout<<"Intersection : "<<endl;
+    for(int i=0;i<n2;i++){
+        if(s.find(arr2[i])!=s.end()){
+            cout<<arr2[i]<<" ";
+        }
     }
-    else if(arr1[i]> arr2[j]){
-        j++;
-    }
-    else{// arr1[]==arr2[]
-        cout<<arr1[i]<<" ";
-        i++;
-        j++;
-    }
-}
-return 0;
+    return 0;
 }
