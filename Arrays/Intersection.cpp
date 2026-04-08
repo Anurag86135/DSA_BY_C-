11a26f65e39f551328e9895765290d0943f191d2
Anurag <anuragmishra11904@gmail.com>
2026-04-08 11:16:56 +0530

    Intersection

---
+++ Arrays/Intersection.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<algorithm>
+#include<unordered_set>
+using namespace std;
+
+// int main(){
+//     int arr1[]={1,2,3,4,5};
+//     int arr2[]={2,3,44,56};
+
+//     int n1= sizeof(arr1)/sizeof(arr1[0]);
+//     int n2= sizeof(arr2)/sizeof (arr2[0]);
+
+//     cout<<" Intersection: "<<endl;
+//     for(int i=0;i<n1;i++){
+//         for(int j=0;j<n2;j++){
+//             if(arr1[i] == arr2[j]){
+//                 cout<<arr1[i]<<" ";
+//                 break;
+//             }
+//         }
+//     }
+//     return 0;
+    
+// }
+
+// Optimal Appraoch
+
+int main(){
+int arr1[]={1,2,3,4,5};
+int arr2[]={3,4,5,6,7};
+
+int n1= sizeof(arr1)/sizeof(arr1[1]);
+int n2= sizeof(arr2)/sizeof (arr2[1]);
+
+sort(arr1,arr1+n1);
+sort(arr2,arr2+n2);
+
+int i=0,j=0;
+cout<<" Intersection: "<<endl;
+while(i<n1 && j<n2){
+    if(arr1[i] < arr2[j]){
+        i++;
+    }
+    else if(arr1[i]> arr2[j]){
+        j++;
+    }
+    else{// arr1[]==arr2[]
+        cout<<arr1[i]<<" ";
+        i++;
+        j++;
+    }
+}
+return 0;
+}
